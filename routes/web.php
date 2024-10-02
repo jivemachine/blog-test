a694e46cc205ec0ec9e2edb692e4c542b5eba8fc
jivemachine <the.jeremy.cobb@gmail.com>
2024-10-01 19:12:06 -0500

    added routes & react components (test)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
+use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return Inertia::render('Index', [
+        'foo' => 'bar',
+    ]);
 });
+
+
+Route::get('/about', function () {
+    return Inertia::render('About');
+});
+
+Route::get('/contact', function () {
+    return Inertia::render('Contact');
+});
+
+Route::get('/posts', function () {
+    return Inertia::render('Posts');
+});
+
+
+// Route::resource('/post', PostController::class);
